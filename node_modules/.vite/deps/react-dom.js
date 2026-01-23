6bb26bb6fc422af39175cf29eab4f1a6d1968b49
JimK <132590388+web-dev-pr0@users.noreply.github.com>
2026-01-23 12:04:36 +0200

    Delete node_modules directory

--- node_modules/.vite/deps/react-dom.js
+++
@@ -1,4 +0,0 @@
-import "./react-B5JTGcpc.js";
-import { t as require_react_dom } from "./react-dom-c5mxk89o.js";
-
-export default require_react_dom();
--- node_modules/.vite/deps/react.js
+++
@@ -1,3 +0,0 @@
-import { t as require_react } from "./react-B5JTGcpc.js";
-
-export default require_react();
